86a54654ee625754256eab06c88fcd1b8c9601b3
lukebrazil <luke.brazil13@icloud.com>
2021-05-20 15:20:43 -0500

    car

--- com/qa/garage/Car.java
+++ com/qa/garage/Car.java
@@ -1,5 +1,24 @@
 package com.qa.garage;
 
-public class Car {
+public class Car extends Vehicle implements DrivingBehavior{
+
+	public Car(String manufacturer, String model, int year) {
+		super(manufacturer, model, year);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void accelerate() {
+		// TODO Auto-generated method stub
+		System.out.println("Car is accelerating...");
+	}
+
+	@Override
+	public void brakeing() {
+		// TODO Auto-generated method stub
+		System.out.println("Car is brakeing...");
+	}
+	
+	
 
 }
